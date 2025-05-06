ed86cdc15c5e314ad902b203b7f5bfc3b344cb82
29722 <zhang18210513357@gmail.com>
2025-05-06 18:36:16 +0800

    更改

---
+++ cmake-build-debug-visual-studio-1/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ResumeAnalyzer_FILE E:/CLion_Object/ResumeAnalyzer/cmake-build-debug-visual-studio-1/ResumeAnalyzer.exe)
+set(__QT_DEPLOY_TARGET_ResumeAnalyzer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ResumeAnalyzer_RUNTIME_DLLS E:/Qt/6.9.0/msvc2022_64/bin/Qt6Sqld.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6Chartsd.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgetsd.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6OpenGLd.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6Guid.dll;E:/Qt/6.9.0/msvc2022_64/bin/Qt6Cored.dll)
